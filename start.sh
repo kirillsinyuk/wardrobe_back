8df9f58c1cab1cd5762f86c63c071cacd2b0b192
SinyukKirill <kirill.s.works@gmail.com>
2020-04-18 00:16:14 +0300

    wait-for-mysql added

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ cd ../
 
 cd api-service
 mvn clean install
-docker build -t api-service:latest .
+docker build -t api-service .
 cd ../
 
 cd parser
 mvn clean install
-docker build -t parser:latest .
+docker build -t parser .
 cd ../
 
 docker-compose up
